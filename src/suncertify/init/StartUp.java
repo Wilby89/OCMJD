1b9114d78ee76d0ab262e1218e48407f507c1204
ewibros <ewibros@EV68B599FB8E86.eemea.ericsson.se>
2013-02-04 11:18:27 +0000

    Initial HEAD commit

---
+++ src/suncertify/init/StartUp.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suncertify.init;
+
+/**
+ *
+ * @author ewibros
+ */
+public class StartUp {
+    
+}
