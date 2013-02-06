730eb1999ef8ec8435abf313808c7b95b2263f63
ewibros <ewibros@EV68B599FB8E86.eemea.ericsson.se>
2013-02-06 13:59:46 +0000

    Database cleanup

--- src/suncertify/db/LoadDatabase.java
+++ src/suncertify/db/LoadDatabase.java
@@ -35,9 +35,8 @@ public class LoadDatabase {
             System.out.println("Name Size: "+nameSize);
             byte[] fieldName = new byte[nameSize];
             fileObject.read(fieldName);
-            System.out.println(new String(fieldName));
-            fileObject.readByte(); //fieldLength, not needed to display
-            
+            System.out.println(new String(fieldName, "US-ASCII")); //The character encoding is 8 bit US ASCII
+            fileObject.readByte(); //fieldLength, not needed to display            
         }
         System.out.println("===========Records============");
         try {
@@ -45,46 +44,12 @@ public class LoadDatabase {
                 fileObject.readByte();
                 byte[] fieldName = new byte[159]; //length of db field names, 64+64+4+1+8+10+8 = 159
                 fileObject.read(fieldName);
-                System.out.println(new String(fieldName)); 
+                System.out.println(new String(fieldName, "US-ASCII")); //The character encoding is 8 bit US ASCII
             }
         } catch (EOFException e) {
             System.out.println("End of file");
         }
         fileObject.close();
         
-    }
-    
-    public static void loadDatabase2() throws FileNotFoundException, IOException {  
-          
-        RandomAccessFile raf = new RandomAccessFile(new File("db-1x3.db"),"r");  
-        int Cookie = raf.readInt();  
-        int fields = raf.readShort();  
-        System.out.println("Cookie:"+Cookie);  
-        System.out.println("Num of Fields:"+fields);  
-        for(int i=0; i<fields;i++)  
-        {  
-            System.out.println("---- Column Description ----");  
-            int nameSize = raf.readUnsignedByte();  
-            System.out.println("Desc Size:"+nameSize);  
-            byte[] buff=new byte[nameSize];  
-            raf.read(buff);  
-            System.out.println(new String(buff,"US-ASCII"));  
-            System.out.println("Value Size:"+raf.readUnsignedByte());  
-            System.out.println("---- Column Ends ----");  
-        } 
-        System.out.println("++++ Records ++++");  
-        try{  
-            while(true){  
-                byte valid = raf.readByte();  
-                System.out.print(valid+" - ");  
-                byte buff[] = new byte[159];  
-                raf.read(buff);  
-                String s =  new String (buff,"US-ASCII");  
-                System.out.println(s);  
-                  
-            }  
-        }catch (EOFException exc){}  
-        raf.close();  
-    }  
-    
+    }    
 }
